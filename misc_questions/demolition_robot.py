bcee83b27d443bff19922463911df06255335741
Jansen Yan <jansen@home-campus.com>
2021-07-13 21:56:48 -0700

    add solution for robot problem using DFS, but still need to optimize

--- misc_questions/demolition_robot.py
+++ misc_questions/demolition_robot.py
@@ -3,7 +3,7 @@ Given a matrix with values 0 (trenches) , 1 (flat) , and 9 (obstacle) you have t
 The demolition robot must start at the top left corner of the matrix, which is always flat, and can move on block up, down, right, left.
 The demolition robot cannot enter 0 trenches and cannot leave the matrix.
 
-Assumptions made: topleft corner of the grid is always 1, and there is always a 9 within the array somewhere (doesn't necessarily have to be reachable)
+Assumptions made: there is always a 9 within the array somewhere (doesn't necessarily have to be reachable)
 Sample Input :
 [1, 0, 0],
 [1, 0, 0],
@@ -16,36 +16,48 @@ class Solution:
 		return self.dfs(grid, 0,0)
 
 	def dfs(self, grid: [[int]], i: int, j: int) -> int:
-		total = 0
+		total = float('inf') 
 		min_total = float('inf')
 		if (grid[i][j] == 9):
 			return 0
 		## down
 		temp = grid[i][j]
 		grid[i][j] = 0
+		print('current index: ', i, j)
 		if (0 <= i+1 < len(grid) and 0 <= j < len(grid[0]) and grid[i+1][j] != 0):
 			print('going down to ', grid[i+1][j])
-			total = 1 + self.dfs(grid, i+1, j)
+			total = min(total, 1 + self.dfs(grid, i+1, j))
 		## up
 		if (0 <= i-1 < len(grid) and 0 <= j < len(grid[0]) and grid[i-1][j] != 0):
 			print('going up to ', grid[i-1][j])
-			total = 1 + self.dfs(grid, i-1, j)
+			total = min(total, 1 + self.dfs(grid, i-1, j))
 		## left
 		if (0 <= i < len(grid) and 0 <= j+1 < len(grid[0]) and grid[i][j+1] != 0):
-			print('going left to ', grid[i][j+1])
-			total = 1 + self.dfs(grid, i, j+1)
+			print('going right to ', grid[i][j+1])
+			total = min(total, 1 + self.dfs(grid, i, j+1))
 		## right
 		if (0 <= i < len(grid) and 0 <= j-1 < len(grid[0]) and grid[i][j-1] != 0):
-			print('going right to ', grid[i][j-1])
-			total = 1 + self.dfs(grid, i, j-1)
+			print('going left to ', grid[i][j-1])
+			total = min(total, 1 + self.dfs(grid, i, j-1))
 		grid[i][j] = temp
 		print('total: ', total)
 		return total
 
 if __name__ == '__main__':
 	s = Solution()
+	## 3x3
 	grid1 = [[1,0,0],[1,0,0],[1,9,1]]
+	## 4x4
 	grid2 = [[1,0,0,1],[1,1,1,1],[0,1,9,0]]
-	grid3 = [[1,0,0],[1,0,0],[1,9,1]]
+	## 5x5
+	grid3 = [[1,1,1,9,0],[1,1,0,1,0],[1,1,1,1,0],[1,0,0,1,0],[1,1,1,1,0]]
+	## no obstacle
+	grid4 = [[1,0,0],[1,0,0],[1,0,1]]
+	print('---------------grid1-------------\n')
 	print(s.robot(grid1))
+	print('---------------grid2-------------\n')
 	print(s.robot(grid2))
+	print('---------------grid3-------------\n')
+	print(s.robot(grid3))
+	print('---------------grid4-------------\n')
+	print(s.robot(grid4))
