54db0e454089f7729692972acae94e1f642514b0
jyan500 <jyan500@gmail.com>
2023-07-04 12:08:55 -0700

    revisted group anagrams

--- blind 75 leetcode/Arrays and Strings/group_anagrams.py
+++ blind 75 leetcode/Arrays and Strings/group_anagrams.py
@@ -5,6 +5,21 @@ An Anagram is a word or phrase formed by rearranging the letters of a different 
 https://leetcode.com/problems/group-anagrams/
 
 '''
+class Solution2:
+	# revisited on 7/4/2023
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # sort each word by letter and use it as a key in a dict so we can keep track
+        # of which words we've already determined as anagrams
+        # if two sorted words are the same = anagrams of each other
+        groups = dict()
+        for word in strs:
+            sortedWord = "".join(sorted(list(word)))
+            if sortedWord in groups:
+                groups[sortedWord].append(word)
+            else:
+                groups[sortedWord] = [word]
+        return [groups[key] for key in groups]
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         lookup = dict()
