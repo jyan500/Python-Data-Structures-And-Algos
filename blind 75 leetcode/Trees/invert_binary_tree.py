2e62c837cee34ef59388b47cd0f65b4dccd7fd24
jyan500 <jyan500@gmail.com>
2023-07-16 16:04:30 -0700

    revisit invert binary tree

--- blind 75 leetcode/Trees/invert_binary_tree.py
+++ blind 75 leetcode/Trees/invert_binary_tree.py
@@ -8,6 +8,21 @@ https://leetcode.com/problems/invert-binary-tree/
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+"""
+Revisited on 7/16/2023
+O(N) time and O(1) space
+"""
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root:
+            # swap the values of the nodes on the left and right
+            root.left, root.right = root.right, root.left
+            # traverse the remainder of the tree
+            self.invertTree(root.left)
+            self.invertTree(root.right)
+            return root
+
 class Solution:
     def invertTree(self, root: TreeNode) -> TreeNode:
         if (root):
