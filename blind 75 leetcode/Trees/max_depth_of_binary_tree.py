47604808a75b5affff366f812814dce7002ff82b
jyan500 <jyan500@gmail.com>
2023-07-17 14:48:08 -0700

    revisit max depth of binary tree

--- blind 75 leetcode/Trees/max_depth_of_binary_tree.py
+++ blind 75 leetcode/Trees/max_depth_of_binary_tree.py
@@ -11,6 +11,23 @@ https://leetcode.com/problems/maximum-depth-of-binary-tree/
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+"""
+Slightly different solution, revisited on 7/17/2023
+O(N) time, O(1) space
+track a depth variable and continually increment it until the root is none,
+then return it in the base case
+continually take the max between the depths found on the left and right subtrees
+"""
+class Solution2:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        return self.maxDepthHelper(root, 0)
+    def maxDepthHelper(self, root: Optional[TreeNode], depth: int) -> int:
+        if not root:
+            return depth
+        else:
+            return max(self.maxDepthHelper(root.left, depth + 1), self.maxDepthHelper(root.right, depth + 1))
+
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
         if (root is None):
