fba83e9cbd7c38dbdb9c84d8c4b1198db85b03c3
jyan500 <jyan500@gmail.com>
2023-07-19 14:26:52 -0700

    revisited course schedule

--- blind 75 leetcode/Searching with 2D Array and Graphs/course_schedule.py
+++ blind 75 leetcode/Searching with 2D Array and Graphs/course_schedule.py
@@ -9,6 +9,69 @@ Create adjacency list using a dict, mapping the course as the key, and the prere
 loop through all courses and run DFS on each course, keeping track of visited set
 within DFS, 
 '''
+
+'''
+Revisited on 7/19/2023
+https://www.youtube.com/watch?v=EgI5nU9etnU&ab_channel=NeetCode
+
+Cycle Detection
+O(N+P) where N is the amount of courses and P is the amount of prerequisites for the course
+Space is O(N+P) to hold all courses and prereqs in the adjacency list
+Key concepts:
+1) creating the adjacency list based on the prereqs, where the key is the course
+and the value is the list of prereqs
+
+when we say [2,1], this means course 1 is a prereq for course 2, so 
+the graph visual would be 2 --> 1
+
+2) Applying DFS, but keeping track of a visited set to track cycles. If at any point, we're at a given course
+but it's already in the visited set, this is a cycle.
+
+3) The base case is that a course has no prereqs, which means 
+the course can be taken at any time, return True
+
+4) After visiting all of a course's prereqs within DFS, once the recursive case goes back to the given course,
+we can just remove the prereqs from the list of prereqs to avoid extra recursive calls
+
+'''
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # create adjacency list where the key is the class
+        # and the value is a list of the prereqs
+        prereqMap = dict()
+        for i in range(numCourses):
+            prereqMap[i] = []
+        for i in range(len(prerequisites)):
+            course, prereq = prerequisites[i]
+            prereqMap[course].append(prereq)
+        visited = set()
+        def dfs(course: int):
+            # if this course was already visited
+            # this is a cycle, so course cannot be completed
+            if course in visited:
+                return False
+            # if the course does not have prereqs, return True
+            if prereqMap[course] == []:
+                return True
+            # add course to visited set
+            visited.add(course)
+            prereqs = prereqMap[course]
+            for prereq in prereqs:
+                # if any of the prereqs cannot be completed, return False
+                if not dfs(prereq):
+                    return False
+            # afte visiting all prereqs for this course, 
+            # remove the course from the visited set
+            # and remove all prereqs to avoid iterating through them again accidentally 
+            visited.remove(course)
+            prereqMap[course] = []
+            return True
+        
+        for course in prereqMap:
+            if not dfs(course):
+                return False
+        return True
+
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         ## the idea is basically a cycle detection algorithm using DFS
