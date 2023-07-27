886bf98eb5fb8476f5e2a14f28d7559a3c1ecdb6
jyan500 <jyan500@gmail.com>
2023-07-27 11:22:44 -0700

    added solutions for ransom note and longest palindrome

--- blind 75 leetcode/Dynamic Programming and Recursion/climbing_stairs.py
+++ blind 75 leetcode/Dynamic Programming and Recursion/climbing_stairs.py
@@ -4,6 +4,24 @@ You are climbing a staircase. It takes n steps to reach the top.
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 https://leetcode.com/problems/climbing-stairs/
 '''
+
+"""
+Recursive Fibonacci with Memoization
+"""
+class Solution2:
+    def climbStairs(self, n: int) -> int:
+        res = {1:1, 2:2}
+        def helper(n):
+            if n == 1:
+                return 1
+            if n == 2:
+                return 2
+            prev1 = res[n-1] if n-1 in res else helper(n-1)
+            prev2 = res[n-2] if n-2 in res else helper(n-2)
+            res[n] = prev1 + prev2
+            return res[n]
+        return helper(n)
+
 class Solution:
 	## iterative fibonacci solves this problem, since the number of ways to get 
 	## to the current step is the previous two numbers added together, which is modeled by the fibonacci sequence
