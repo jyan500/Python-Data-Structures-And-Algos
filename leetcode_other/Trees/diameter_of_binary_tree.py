801c592c904aa509b6878456fb6e4791d864b9a3
jyan500 <jyan500@gmail.com>
2023-07-28 10:47:58 -0700

    revisited and added explanation for diameter of binary tree

--- leetcode_other/Trees/diameter_of_binary_tree.py
+++ leetcode_other/Trees/diameter_of_binary_tree.py
@@ -17,10 +17,61 @@ issue is that there's a lot of repeated work
 Space : O(1)
 
 Alternative O(N) solution
-the diameter of the binary tree is just the depths of the left and right subtrees added together
+the diameter of the binary tree is just the depths of the left and right subtrees added together,
+so while we calculate the height of the left and right subtrees, we can calculate the max diameter
+at the same time (essentially doing it bottom up starting from the leaf, instead of top down starting from the root)
 
-so alternatively, we can just modify our getDepth function so that it will calculate the diameter at each
-point and return the max so we don't have to recur into each node
+A key concept behind this is using a global variable to simplify the recursion, in this case, we need to return the 
+height of the subtree in the recursive call, but we also need to track diameter. We can keep track of max diameter
+via the global variable.
+
+[6, 3, 10, 4, 7, null, null, null, null, 2, 8, 1, null, null, null, null, 5, null]
+
+        6
+    3       10 
+  4   7     
+     2 8 
+    1
+     5  
+
+In this example, we can see the diameter of the tree is 6, starting to the left
+of the root 6 (6 -> 3 -> 4, which is 2) (3->7->2->1->5, which is 4)
+
+1) root = 6 left_depth = self.getDepth(3)
+2) root = 3, left_depth = self.getDepth(4)
+3) root = 4, left_depth = self.getDepth(None) -> base case, returns 0 
+
+back to recursive call 2)
+root = 3
+left_depth = 0
+right_depth = self.getDepth(7)
+
+4) root = 7, left_depth = self.getDepth(2)
+5) root = 2, left_depth = self.getDepth(1)
+6) root = 1, , no left child,  right_depth = self.getDepth(5)
+7) root = 5, no left and right child, returns 0 
+
+
+Back to recursive call 6), no right child, diameter is 1, returns 1
+Back to recursive call 5), no right child, diameter is 2, returns 2
+Back to recursive call 4), has right child
+
+root = 7
+left_depth = 0
+right_depth = self.getDepth(8)
+
+8) root = 8, right_depth = self.getDepth(None) -> base case, returns 0 
+right_depth is 1
+
+
+back to recursive call 4)
+diameter is left + right (which is 3+1), returns tree height of 3
+
+Back to recursive call 2)
+increase tree height to 4, maxDiameter is now 4 + 1 = 5 (1 being the left subtree of 3)
+
+pops back to recursive call 1)
+maxDiameter is now 6, tree height is 5
 
 '''
 # Definition for a binary tree node.
@@ -45,6 +96,7 @@ class Solution:
         if (root == None):
             return 0
         else:
+            # calculate the height of the left subtree and right subtree
             left_depth = self.getDepth(root.left)
             right_depth = self.getDepth(root.right)
             ## since the diameter is the left subtree depth + right subtree depth, add together and set the max
