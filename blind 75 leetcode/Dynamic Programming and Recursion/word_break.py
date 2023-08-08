ed254995f2529262312c9aad76dc967ed393ba6d
jyan500 <jyan500@gmail.com>
2023-08-08 12:30:08 -0700

    revisited word break

--- blind 75 leetcode/Dynamic Programming and Recursion/word_break.py
+++ blind 75 leetcode/Dynamic Programming and Recursion/word_break.py
@@ -1,6 +1,17 @@
 '''
 https://leetcode.com/problems/word-break/
 
+Revisited on 8/8/2023
+Time complexity of the memoized solution O(N^3), N^2 for the recursion, and N for the split method on the string each time
+Space complexity is O(N) for the memoization, where N is the length of the input string
+
+Key Concept:
+1) Pick a word in our word dict and then shorten our input string if the first n characters of our input string match the word
+2) In our memoization, we store whether the given input string in the recursive call can be split or not
+based on the result of self.dfs(),
+3) we usually put the call to memoization outside
+the loop (where the base case is normally) so we can prevent additional loops
+
 '''
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
