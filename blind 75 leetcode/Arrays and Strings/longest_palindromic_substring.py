b6158e75236872f6b413d1440a4f23ada53a72ec
jyan500 <jyan500@gmail.com>
2023-08-09 13:47:15 -0700

    longest palindromic substring

--- blind 75 leetcode/Arrays and Strings/longest_palindromic_substring.py
+++ blind 75 leetcode/Arrays and Strings/longest_palindromic_substring.py
@@ -22,6 +22,51 @@ Input: s = "ac"
 Output: "a"
 '''
 
+"""
+Revisited on 8/9/2023
+Similar solution as below
+Time complexity: O(N^2), for each ith character, we evaluate all other characters in the string
+Space complexity: O(N), it's possible that the palindrome is the entire length of the original string
+"""
+class Solution2:
+    def longestPalindrome(self, s: str) -> str:
+        maxLength = float("-inf")
+        substring = ""
+        for i in range(len(s)):
+            # check for odd length palindrome,
+            # where l = i - 1, one element to the left
+            # and r = i + 1, one element to the right,
+            # and then move both pointers outwards
+            l = i-1
+            r = i+1
+            while True:
+                if l < 0 or r > len(s) - 1 or s[l] != s[r]:
+                    # length of substring is the (right index - 1) - (left index + 1) + 1
+                    if r-l+1> maxLength:
+                        maxLength = r-l+1
+                        # because we extended one past the boundary, we need to add
+                        # one back to l
+                        substring = s[l+1:r]
+                    break
+                else:
+                    l -= 1
+                    r += 1     
+            # check for even length palindrome, where l = i, and r would be one element to the right
+            # and move both pointers outwards
+            l = i
+            r = i+1
+            while True:
+                if l < 0 or r > len(s) - 1 or s[l] != s[r]:
+                    if r-l+1 > maxLength:
+                        maxLength = r-l+1
+                        substring = s[l+1:r]
+                    break
+                else:
+                    l -= 1
+                    r += 1
+        
+        return substring
+            
 def longestPalindrome(self, s: str) -> str:
         def longestAtIndex(s, left, right):
             ## starting from the left and right pointers, expand outwards <-- -->
