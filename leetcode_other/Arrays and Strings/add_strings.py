69ab77ad1686000e3831ad9740ef2acabe7aa61b
jyan500 <jyan500@gmail.com>
2023-10-05 11:18:03 -0700

    add strings solution

--- leetcode_other/Arrays and Strings/add_strings.py
+++ leetcode_other/Arrays and Strings/add_strings.py
@@ -2,6 +2,55 @@
 https://leetcode.com/problems/add-strings/
 https://leetcode.com/problems/add-strings/solution/
 '''
+
+class Solution2:
+    """
+    10/5/2023
+    
+    1) This problem is greatly simplified by just padding zeroes to the smaller
+    number so that both numbers are also equal in length. This simplifies
+    carryover situations.
+
+    We can just loop through the numbers in reverse and add the digits, if there's carryover,
+    it'll be set to 1 ( can never be greater than that ), and just add the digit to our result string
+
+    The only edge case is when there's one final carryover, i.e 
+    1 + 9,
+    
+    1
+      1
+      9
+      0
+
+    add the final "1" to form 10
+
+    O(N) time
+    O(N) space
+
+    """
+    def addStrings(self, num1: str, num2: str) -> str:
+        # pad the smaller number with zeroes to simplify carryover
+        numZeroes = abs(len(num1)-len(num2))
+        if len(num1) < len(num2):
+            num1 = ("0" * numZeroes) + num1
+        else:
+            num2 = ("0" * numZeroes) + num2
+        carryover = 0
+        res = ""
+        for i in range(len(num1)-1,-1,-1):
+            digit = int(num1[i]) + int(num2[i]) + carryover
+            if digit >= 10:
+                carryover = 1
+                res = str(digit-10) + res
+            else:
+                carryover = 0
+                res = str(digit) + res
+        # if both numbers have the same amount of digits
+        # and there's carryover, add the final carryover
+        if carryover != 0:
+            res = str(carryover) + res
+        return res
+
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
         ## initial thoughts
