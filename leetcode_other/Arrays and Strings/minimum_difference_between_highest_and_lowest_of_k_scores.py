768bab29107a1b44afca14e7bd07c649f746f558
jyan500 <jyan500@gmail.com>
2023-10-31 10:42:25 -0700

    min difference between heighest and lowest scores

---
+++ leetcode_other/Arrays and Strings/minimum_difference_between_highest_and_lowest_of_k_scores.py
@@ -0,0 +1,22 @@
+"""
+https://leetcode.com/problems/minimum-difference-between-highest-and-lowest-of-k-scores/
+https://www.youtube.com/watch?v=JU5XdBZZtlk&ab_channel=NeetCode
+
+Key Concept:
+To get the minimum difference between scores, it'd make sense to sort them first.
+And then apply sliding window of size K, where you continually
+take the difference between the left side of the window (smallest) and the right side of the window (largest)
+until you find the minimum difference.
+
+"""
+class Solution:
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        l = 0
+        r = k - 1
+        res = float("inf")
+        while (r < len(nums)):
+            res = min(nums[r] - nums[l], res)
+            r += 1
+            l += 1
+        return res
