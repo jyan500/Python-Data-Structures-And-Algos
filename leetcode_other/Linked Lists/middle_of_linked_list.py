6ec52fb4f9122c061e9b19bc5375d02d7c2f8bc5
jyan500 <jyan500@gmail.com>
2023-11-22 10:30:52 -0800

    middle of linked list

--- leetcode_other/Linked Lists/middle_of_linked_list.py
+++ leetcode_other/Linked Lists/middle_of_linked_list.py
@@ -10,6 +10,63 @@ so mid + 1
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+"""
+O(N) Time O(1) Space
+11/22/2023
+Using the cycle detection algorithm,
+with slow and fast pointer.
+
+If the fast pointer reaches the end of the list, that means it is an even number of nodes.
+If it is none, that means it's odd number. In that case, we need to 
+make sure to return the "second" middle node, so in the case of even numbered nodes,
+return slow.next, and for odd numbered, return slow
+
+Odd Numbered:
+
+1 -> 2 -> 3 -> 4 -> 5 -> None
+^    ^
+slow fast
+
+1 -> 2 -> 3 -> 4 -> 5 -> None
+     ^         ^
+    slow      fast
+
+1 -> 2 -> 3 -> 4 -> 5 ->None
+          ^               ^
+         slow           fast
+
+Here the middle is slow, so return slow
+
+Even Numbered
+1 -> 2 -> 3 -> 4 -> 5 -> 6 -> None
+^    ^
+slow fast
+
+1 -> 2 -> 3 -> 4 -> 5 -> 6 -> None
+    ^          ^
+    slow      fast
+
+1 -> 2 -> 3 -> 4 -> 5 -> 6 -> None
+          ^              ^
+          slow          fast
+
+here, the proper middle is 4, so return slow.next instead of slow
+"""
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head.next
+        while (fast != None and fast.next != None):
+            slow = slow.next
+            fast = fast.next.next
+        return slow.next if fast != None else slow
+
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         cur1 = head
