b01736e352dde28e9affae62d965ed6fffa75129
jyan500 <jyan500@gmail.com>
2023-12-13 14:10:06 -0800

    best time to buy and sell stock ii

---
+++ leetcode_other/Arrays and Strings/best_time_to_buy_and_sell_stock_ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        """
+        https://www.youtube.com/watch?v=3SJ3pUkPQMc&ab_channel=NeetCode
+        Concept:
+        1) As seen in the neetcode vid, we only turn a profit when 
+        going from a lower to a higher price.
+        2) Loop through the array and see if the previous price is higher, if so
+        add the difference between the current and previous price to our total profit
+        """
+        total = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                total += prices[i] - prices[i-1]
+        return total
