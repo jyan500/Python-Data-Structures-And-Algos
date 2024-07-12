9bbb9df2a4348813a5ab7a587a729d7f44d85a58
jyan500 <jyan500@gmail.com>
2024-07-12 11:10:48 -0700

    added djikstra's solution for cheapest flights, and no extra memory solution for add two numbers

--- node_js_ds_and_algos_implementations/graphs/cheapest_flights_in_k_stops.js
+++ node_js_ds_and_algos_implementations/graphs/cheapest_flights_in_k_stops.js
@@ -32,3 +32,55 @@ var findCheapestPrice = function(n, flights, src, dst, k) {
     }
     return prices[dst] !== Number.POSITIVE_INFINITY ? prices[dst] : -1
 };
+
+/**
+ * @param {number} n
+ * @param {number[][]} flights
+ * @param {number} src
+ * @param {number} dst
+ * @param {number} k
+ * @return {number}
+ */
+var findCheapestPrice = function(n, flights, src, dst, k) {
+    // Modified Djikstra's Algorithm
+    // By @PippyPattyPatterson, from youtube comment on Neetcode's Channel for
+    // the solution to this problem: https://www.youtube.com/watch?v=5eIK3zUdYmE&ab_channel=NeetCode
+    // the difference is that we need to account for the amount of stops taken, both in the min heap,
+    // and also the visited dictionary (vertexToSteps)
+    // Time Complexity: O(E*LogV)
+    // Space: O(E+V)
+    let adjacency = {}
+    for (let i = 0; i < n; ++i){
+        adjacency[i] = []
+    }
+    for (let [from, to, price] of flights){
+        adjacency[from].push([to, price])
+    }
+    let heap = new MinPriorityQueue({priority: (element) => element[0]})
+    // enqueue the weights (price), num steps taken, and current node (which is src initially)
+    // the num steps starts at -1 since 
+    // one stop (k) travels across two cities, so it's actually k + 1
+    heap.enqueue([0, -1, src])
+    // this acts as a "memoization", mapping
+    // the current node to the amount of steps needed to reach it from src
+    let vertexToSteps = {}
+    while (!heap.isEmpty()){
+        let [weight, steps, node] = heap.dequeue().element
+        // if we've surpassed k or we've visited
+        // this node AND we've surpassed k in this path, continue
+        if (steps > k || (node in vertexToSteps && steps > vertexToSteps[node])){
+            continue
+        }
+        vertexToSteps[node] = steps
+        if (node === dst){
+            return weight
+        }
+        // iterating through all neighbors
+        for (let [dest, destWeight] of adjacency[node]){
+            let element = [destWeight + weight, steps + 1, dest]
+            heap.enqueue(element)
+        }
+    }
+    // if dest cannot be reached in k stops, return -1
+    return -1
+};
--- node_js_ds_and_algos_implementations/linked lists/add_two_numbers.js
+++ node_js_ds_and_algos_implementations/linked lists/add_two_numbers.js
@@ -10,6 +10,95 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
+/*
+Approach without using extra memory:
+similar to add two strings where you pad the shorter number with zeroes
+to avoid edge cases
+With this problem, you can start iterating from the front of the linked list since
+the digits are stored in reverse, so it's the same order when performing elementary addition normally.
+
+Time: O(N)
+Space: O(1)
+*/
+var addTwoNumbers = function(l1, l2) {
+
+    let dummy = new ListNode(0)
+    let curr = dummy
+    let carryover = 0
+    let temp1 = l1
+    let temp2 = l2
+    let len1 = 0
+    let len2 = 0
+    while (temp1.next){
+        ++len1
+        temp1 = temp1.next
+    }
+    while (temp2.next){
+        ++len2
+        temp2 = temp2.next
+    }
+    /* since the loops above only go to the second to last element,
+    we can add 1 to both lengths for correctness sake, but it's not really necessary
+    since we only care about the difference in length and not the exact lengths of both LL's
+    */
+    ++len1
+    ++len2
+    
+    if (len1 !== len2){
+        /* insert zeroes to account for the difference in length
+        note that because the digits are in reverse, we'd want to pad
+        the zeroes at the end of the linked list (i.e 999 + 10, would be [9, 9, 9], and [0, 1] in LL form)
+        to avoid edgecases, we'd want [9, 9, 9] and [0, 1, 0], with the zero padded at the end
+        we'd get [9, 0, 0, 1] once performing the addition
+        */
+        if (len1 > len2){
+            let numZeroes = len1 - len2
+            // insert new LL nodes with value 0 at the end of the shorter LL
+            for (let i = 0; i < numZeroes; ++i){
+                temp2.next = new ListNode(0)
+                temp2 = temp2.next
+            }
+        }
+        else {
+            let numZeroes = len2 - len1
+            for (let i = 0; i < numZeroes; ++i){
+                temp1.next = new ListNode(0)
+                temp1 = temp1.next
+            }
+        }
+    }
+    
+    /*
+    because the digits are already stored in reverse order,
+    we can start iterating and performing adding/carryover operations.
+    We don't need to worry about the numbers not having the same amount of digits
+    and dealing with edgecases there since we did the padding operation above.
+    
+    similar to elementary addition, add the two digits at the current spot + carryover,
+    if sum >= 10, set carryover to 1 and then put the sum - 10 as the value of the LL node
+    else, set carryover back to 0 and put the sum as the value of the LL node
+    */
+    while (l1 && l2){
+        let sum = carryover + l1.val + l2.val
+        if (sum >= 10){
+            carryover = 1
+            curr.next = new ListNode(sum - 10)
+        }
+        else {
+            carryover = 0
+            curr.next = new ListNode(sum)
+        }
+        curr = curr.next
+        l1 = l1.next
+        l2 = l2.next
+    }
+    // if there's still carryover, add one last LL node with the carryover value
+    if (carryover === 1){
+        curr.next = new ListNode(carryover)
+    }
+    return dummy.next
+};
+
 var addTwoNumbers = function(l1, l2) {
     /*
     pull both numbers out into an array
