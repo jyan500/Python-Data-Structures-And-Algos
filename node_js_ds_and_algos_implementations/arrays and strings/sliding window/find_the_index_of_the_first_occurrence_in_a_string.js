036658627667197400fc5ae68ebc4edb027ccd71
jyan500 <jyan500@gmail.com>
2024-08-27 10:29:34 -0700

    find index of the first occurrence in a string

--- node_js_ds_and_algos_implementations/arrays and strings/sliding window/find_the_index_of_the_first_occurrence_in_a_string.js
+++ node_js_ds_and_algos_implementations/arrays and strings/sliding window/find_the_index_of_the_first_occurrence_in_a_string.js
@@ -5,6 +5,20 @@ Time Complexity:
 O(N*M), where M is the size of the needle and N is the size of the haystack
 
 */
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    for (let i = 0; i <= haystack.length - needle.length; ++i){
+        let sub = haystack.slice(i, i + needle.length)
+        if (sub === needle){
+            return i
+        }
+    }
+    return -1
+};
 
 /**
  * @param {string} haystack
