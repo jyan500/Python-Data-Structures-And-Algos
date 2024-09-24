f5ab0ba7d33eb9cc48193714f2da0d3e07e7b969
jyan500 <jyan500@gmail.com>
2024-09-24 16:27:15 -0700

    two sum ii, integer input is sorted revisited

--- node_js_ds_and_algos_implementations/arrays and strings/two pointers/two_sum_ii_input_array_is_sorted.js
+++ node_js_ds_and_algos_implementations/arrays and strings/two pointers/two_sum_ii_input_array_is_sorted.js
@@ -1,3 +1,37 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        /*
+        Using two pointers, because the array is already sorted in ascending order,
+        and we know that the first index is always less than the second,
+        we can start one pointer at the left, and one pointer at the right.
+        We then check the sum of the values of the left and right. If its bigger than the target,
+        we have to shift the right pointer inwards (r--) so the total sum becomes less, since
+        the right side number should be smaller since it's sorted. Vice versa, if the sum
+        is less than the target, we have to move the left side by one to increase the total sum 
+        */
+        let l = 0
+        let r = numbers.length - 1
+        while (l <= r){
+            let sum = numbers[l] + numbers[r]
+            if (sum === target){
+                return [l+1,r+1]
+            }
+            else if (sum < target){
+                ++l
+            }
+            else if (sum > target){
+                --r
+            }
+        }
+        
+    }
+}
+
 /**
  * @param {number[]} numbers
  * @param {number} target
