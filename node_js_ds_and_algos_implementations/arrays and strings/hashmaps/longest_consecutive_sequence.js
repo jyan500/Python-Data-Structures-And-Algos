6de966a1a06e1319b06c86d18725444cc00801bc
jyan500 <jyan500@gmail.com>
2024-09-24 15:31:37 -0700

    longest consecutive sequence

--- node_js_ds_and_algos_implementations/arrays and strings/hashmaps/longest_consecutive_sequence.js
+++ node_js_ds_and_algos_implementations/arrays and strings/hashmaps/longest_consecutive_sequence.js
@@ -1,3 +1,50 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        /*
+        Revisited 9/24/2024
+        the trick is realizing that in a consecutive sequence,
+        the numbers must be increased or decreased by 1 only
+        so given a particular number, we can check if it's value + 1 
+        is present, if so this is a consecutive sequence. The start of a consecutive
+        sequence would indicate that the value - 1 is not present, so it's not in the middle
+        of any consecutive sequence.
+        to decrease lookup times, convert nums to a set
+        loop through nums,
+        check to see if nums[i] - 1 exists, if so this is a start of a consecutive sequence
+            use a while loop to continue checking if the next value in the sequence exists
+            once this loop stops, we track the length of the sequence that we found
+            also check to see if this length is greater than a max length that we've found so far
+
+        */
+        /*
+        edge case where the nums array is empty, we can't assume that the smallest consecutive sequence is 1,
+        so we must return 0
+        */
+        if (nums.length === 0){
+            return 0
+        }
+        let lookup = new Set([...nums])
+        // assumes that a single number is a consecutive sequence of length 1
+        let res = 1
+        for (let i = 0; i < nums.length; ++i){
+            let start = nums[i] - 1
+            if (lookup.has(start)){
+                let total = 1
+                while (lookup.has(start + 1)){
+                    start++
+                    total++
+                }
+                res = Math.max(total, res)
+            }
+        }
+        return res
+    }
+}
+
 /**
  * @param {number[]} nums
  * @return {number}
