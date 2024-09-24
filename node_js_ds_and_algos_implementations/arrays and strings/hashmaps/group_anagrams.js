3db7df785771d6209ece0977eb40afa37b467ab3
jyan500 <jyan500@gmail.com>
2024-09-24 13:26:44 -0700

    revisit group anagrams

--- node_js_ds_and_algos_implementations/arrays and strings/hashmaps/group_anagrams.js
+++ node_js_ds_and_algos_implementations/arrays and strings/hashmaps/group_anagrams.js
@@ -3,6 +3,29 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
+    /*
+    I would first create a key that when sorted, can be used to determine whether two strings are anagrams of each other
+    I would then create a hashmap that stores these sorted keys as keys, and values as an array of the strings that are anagrams of each other
+    Finally I'd iterate through the values of the hashmap to get all the groups and return
+    */
+    let groups = {}
+    for (let str of strs){
+        let key = [...str].sort((a,b) => a.localeCompare(b)).join("")
+        if (!(key in groups)){
+            groups[key] = [str]
+        }
+        else {
+            groups[key].push(str)
+        }
+    }
+    return Object.values(groups)
+};
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
     let c = {}
     for (let s of strs){
         // using the sorted version of the string as the key,
