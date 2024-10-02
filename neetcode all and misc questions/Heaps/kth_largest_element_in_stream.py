b204dc3302da737eafb224ed31164b59bf01bb2c
jyan500 <jyan500@gmail.com>
2024-10-01 21:41:47 -0700

    heap problems

--- neetcode all and misc questions/Heaps/kth_largest_element_in_stream.py
+++ neetcode all and misc questions/Heaps/kth_largest_element_in_stream.py
@@ -29,7 +29,36 @@ After heappop, the 4 gets bubbled up to the root as it's now the min element
 heap = [4, 5, 6]
 
 """
+class KthLargest:
+    import heapq
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.heap = nums
+        heapq.heapify(self.heap)
+        """
+        Revisited on 10/1/2024,
+        Time Complexity:
+        O(NLogN) heapify + O(MLogN), where M is the amount of calls made to self.add()
+        put all items of the stream into the min heap so they are sorted, but then
+        pop out until only k items are left.
+        The idea is that the kth largest element is always the first element of the min heap, 
+        kind of like a "sliding window", where the heap only contains the largest k elements
+        """
+        for i in range(len(nums)-k):
+            heapq.heappop(self.heap)
 
+    def add(self, val: int) -> int:
+        """ 
+        push val to the heap. If the heap is now greater than k, pop out the first value. 
+        Return the first value of the heap. 
+        This also works if we add a value that is less than the top of the heap,
+        since we'd add it the top, and then immediately remove it
+        """
+        heapq.heappush(self.heap, val)
+        if len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        return self.heap[0]
+        
 import heapq
 class KthLargest:
     
--- neetcode all and misc questions/Heaps/last_stone_weight.py
+++ neetcode all and misc questions/Heaps/last_stone_weight.py
@@ -2,6 +2,30 @@
 https://leetcode.com/problems/last-stone-weight/
 """
 class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        """
+        https://neetcode.io/problems/last-stone-weight
+        Use a max heap 
+        continue popping off max heap twice at a time until there's at least one element in the max heap left
+        O(NLogN) + O(MLogN), where M is the amount of times heappush() is called
+        """
+        import heapq
+        # use negative numbers to create a max heap
+        maxHeap = [-stones[i] for i in range(len(stones))]
+        heapq.heapify(maxHeap)
+        # there has to be at least two stones
+        while len(maxHeap) > 1:
+            # convert back to positive numbers
+            stone1 = -1 * heapq.heappop(maxHeap)
+            stone2 = -1 * heapq.heappop(maxHeap)
+            if stone1 > stone2 or stone2 > stone1:
+                # get the weight difference and multiply by -1
+                # to put back onto the max heap
+                newStone = -1 * abs(stone1-stone2)
+                heapq.heappush(maxHeap, newStone)
+        return -1 * maxHeap[0] if len(maxHeap) == 1 else 0
+
+class Solution:
     """
     Max Heap Solution
     """
