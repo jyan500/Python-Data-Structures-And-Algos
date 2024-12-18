8cc3e824c44ecc017446993b28e4da1879cfe99a
jyan500 <jyan500@gmail.com>
2024-12-18 11:39:53 -0800

    all nodes distance k in binary tree

--- neetcode all and misc questions/Trees/all_nodes_distance_k_in_binary_tree.py
+++ neetcode all and misc questions/Trees/all_nodes_distance_k_in_binary_tree.py
@@ -4,6 +4,57 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        """
+        Revisited 12/18/2024
+        Initial thoughts:
+        - convert the binary tree to an undirected graph using an adjacency list,
+        and run BFS starting from the target node. Using the inner for loop in BFS,
+        we make sure that only nodes of a certain distance are on the queue,
+        so once cur == k, the while loop breaks and only the nodes of distance K are left on the queue.
+        
+        Time: O(N) 
+        Space: O(N)
+        """
+        from collections import defaultdict, deque
+        self.adjacency = defaultdict(list)
+        def buildAdjacency(root):
+            if root:
+                if root.left:
+                    # save both ways since it's an undirected graph
+                    self.adjacency[root.val].append(root.left.val)
+                    self.adjacency[root.left.val].append(root.val)
+                if root.right:
+                    self.adjacency[root.val].append(root.right.val)
+                    self.adjacency[root.right.val].append(root.val)
+                buildAdjacency(root.left)
+                buildAdjacency(root.right)
+        buildAdjacency(root)
+        
+        visited = set()
+        q = deque()
+        q.append(target.val)
+        visited.add(target.val)
+        cur = 0
+        while (q and cur < k):
+            for i in range(len(q)):
+                node = q.popleft()
+                for neighbor in self.adjacency[node]:
+                    if neighbor not in visited:
+                        visited.add(neighbor)
+                        q.append(neighbor)
+            cur += 1
+        # at the end, the only values left in the queue should be the proper
+        # distance away from target
+        return list(q)
 
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
