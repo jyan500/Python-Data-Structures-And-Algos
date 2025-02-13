df6111a161b43024ec20365af79f04b23e6088aa
jyan500 <jyan500@gmail.com>
2025-02-13 12:00:08 -0800

    capacity to ship packages within d days

--- neetcode all and misc questions/Arrays and Strings/capacity_to_ship_packages_within_d_days.py
+++ neetcode all and misc questions/Arrays and Strings/capacity_to_ship_packages_within_d_days.py
@@ -1,6 +1,84 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
         """
+        Revisited again on 2/13/2025, was able to fully conceptualize the problem
+        this time without reference, but still messed up an edge case.
+        I wrote the numDays function out in a way that makes more sense to me.
+
+        weights = [1,2,3,4,5,6,7,8,9,10], days = 5
+        worst case: take 10 days, shipping one package at a time, where the max capacity is 10
+        you must have a minimum capacity that's >= the max element in weights
+        best case: capacity = sum(weights), where you ship everything in one day
+
+        By establishing these bounds, we now have a means of applying binary search between
+        the range of min capacity and max capacity
+
+        We also have to define function that calculates the amount of days it'll take to ship everything
+        based on the capacity of the ship, so then once we figure out the amount of days based on capacity,
+        we check whether its either greater or less than our target amount of days.
+
+        If it's greater than our target, we need to increase the capacity so it'll take less days, so search
+        right side. Otherwise, we search the left side to see if we can decrease the capacity in order
+        to get a more optimal answer (since we want the min capacity as our answer.)
+
+        for example:
+        capacity = 11
+        first day: 1 + 2 + 3 + 4 = 10, cannot add any more otherwise > 11
+        second day: 5 + 6, cannot add any more otherwise > 11
+        third day: 7
+        fourth day: 8
+        fifth day: 9
+        sixth day: 10
+
+        so if capacity was 11, it'll take 6 days
+
+
+        """
+        minCapacity = max(weights)
+        maxCapacity = sum(weights)
+
+        def numDays(maxCap):
+            amtDays = 0
+            curCapacity = 0
+            for i in range(len(weights)):
+                curCapacity += weights[i]
+                # if we've exceeded the capacity set, 
+                # reset the capacity to the current weight and
+                # increment days
+                if curCapacity > maxCap:
+                    amtDays += 1
+                    curCapacity = weights[i]
+            # since there are no more weights to add, 
+            # if we're still <= capacity, add this remaining day
+            # for example, after adding iterating the last element in the example above,
+            # we'd still have a curCapacity of 10, so we need to account for this since its <= maxCap
+            if curCapacity <= maxCap:
+                amtDays += 1
+            return amtDays
+        
+        def binarySearch(l, r):
+
+            mid = l + (r-l)//2
+            # if we exhausted the current search space, return the capacity we've chosen
+            if (l >= r):
+                return mid
+            # mid represents the capacity we will choose
+            amtDays = numDays(mid)
+            # if amtDays > max days allowed, need to choose a greater capacity 
+            # to decrease the amtDays
+            if amtDays > days:
+                return binarySearch(mid+1, r)
+            # otherwise, search the other side to decrease capacity and get closer to amount of days
+            # with lower capacity
+            else:
+                return binarySearch(l, mid)
+                            
+        
+        return binarySearch(minCapacity, maxCapacity)
+
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        """
         Revisited on 12/16/2024 (with same solution as below)
         similar concept to koko eating bananas but different calculation concept
         binary search from max(weights) to sum(weights), which is the max amount you could ship,
