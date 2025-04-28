1293c20d04992bb1f1393275e3ec150043d56540
jyan500 <jyan500@gmail.com>
2025-04-28 14:44:24 -0700

    revisit median from data stream

--- neetcode all and misc questions/Heaps/find_median_from_data_stream.py
+++ neetcode all and misc questions/Heaps/find_median_from_data_stream.py
@@ -1,5 +1,41 @@
 import heapq
 import math
+
+# revisited on 4/28/2025 with the same solution as below (without the comments)
+class MedianFinder:
+
+    def __init__(self):
+        self.minHeap = []
+        self.maxHeap = []
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.maxHeap, num * -1)
+        if (len(self.maxHeap) > 0 and len(self.minHeap) > 0):
+            topMax = self.maxHeap[0] * -1
+            topMin = self.minHeap[0] 
+            if topMax > topMin:
+                topMax = heapq.heappop(self.maxHeap) * -1
+                heapq.heappush(self.minHeap, topMax)
+                
+        if abs(len(self.minHeap)-len(self.maxHeap)) > 1:
+            if len(self.minHeap) > len(self.maxHeap):
+                top = heapq.heappop(self.minHeap)
+                heapq.heappush(self.maxHeap, -1 * top)
+            else:
+                top = heapq.heappop(self.maxHeap)
+                heapq.heappush(self.minHeap, -1 * top)
+
+    def findMedian(self) -> float:
+        if len(self.maxHeap) == len(self.minHeap):
+            topMax = self.maxHeap[0] * -1
+            topMin = self.minHeap[0]
+            return (topMax+topMin)/2
+        else:
+            if len(self.minHeap) > len(self.maxHeap):
+                return self.minHeap[0]
+            else:
+                return self.maxHeap[0] * -1
+
 class MedianFinder:
     """
     TLE solution:
