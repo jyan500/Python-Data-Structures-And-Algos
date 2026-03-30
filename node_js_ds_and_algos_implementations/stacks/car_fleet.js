b26ab66b3fae765493ba608eb08ebafa292ce75f
jyan500 <jyan500@gmail.com>
2026-03-30 12:12:20 -0700

    car fleet

--- node_js_ds_and_algos_implementations/stacks/car_fleet.js
+++ node_js_ds_and_algos_implementations/stacks/car_fleet.js
@@ -1,3 +1,63 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target, position, speed) {
+        /*
+        Revisited 3/30/2026
+        this is similar to an intervals problem
+        sort the cars by their position
+
+        then compare current and previous cars based on the time taken
+        to reach the target
+
+        if previous takes less time than the current
+            that means they will form a fleet
+            so combine the two cars and set the speed
+            to the slower speed 
+        
+        target position = cur position + (time * cur speed)
+        solve for time
+        time = (target position - cur position)/speed
+
+        */
+        // sort in reverse order where highest position is first
+        let cars = position.map((pos, index) => {
+            return [
+                pos,
+                speed[index]
+            ]
+        }).sort((a,b) => {
+            if (a[0] < b[0]){
+                return 1     
+            } 
+            else if (a[0] > b[0]){
+                return -1
+            }
+            return 0
+        })
+        let stack = [cars[0]]
+        for (let i = 1; i < cars.length; ++i){
+            let [prevPos, prevSpeed] = stack[stack.length-1]
+            let [curPos, curSpeed] = cars[i]
+            let prevTime = (target - prevPos)/prevSpeed
+            let curTime = (target - curPos)/curSpeed
+            // if it takes less time, it will become a fleet with the car in front of it
+            if (curTime <= prevTime){
+                // merge
+                stack[stack.length-1] = [prevPos, prevSpeed]
+            }
+            else {
+                stack.push(cars[i])
+            }
+        }
+        return stack.length
+    }
+}
+
 /**
  * @param {number} target
  * @param {number[]} position
@@ -52,4 +112,4 @@ var carFleet = function(target, position, speed) {
         }
     }
     return stack.length
-};
+};/2026
