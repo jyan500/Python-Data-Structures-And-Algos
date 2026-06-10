8289ecf8e3ef21be4fc25a719290115dbbbdabf7
jyan500 <jyan500@gmail.com>
2026-06-10 12:04:20 -0700

    longest common subsequence

--- node_js_ds_and_algos_implementations/2D dynamic programming/longest_common_subsequence.js
+++ node_js_ds_and_algos_implementations/2D dynamic programming/longest_common_subsequence.js
@@ -1,3 +1,80 @@
+class Solution {
+    /**
+     * @param {string} text1
+     * @param {string} text2
+     * @return {number}
+     */
+    longestCommonSubsequence(text1, text2) {
+        /*
+        backtracking problem
+        text1= cate
+        text2= dcbatcsjaltde
+
+        cate as a subsequence does exist in test2
+        but if you were to start counting from the first "c"
+        in text2, you would find "cat"
+
+        it seems you will need two pointers
+        one to go through index of text1
+        and the other for index of text2
+
+        However, we can't always assume that the longest subsequence will start at 0
+
+        for example, the longest subsequence here is "s" which is length 1
+        psnw
+        vozsh
+
+        this is why we need backtracking, since the longest common subsequence could be found
+        by starting at the next index for either i or j
+        
+        if text1[i] === text2[j]
+            recursive call advance j and i
+        else
+            recursive call that only advances j and keeps i the same
+            recursive call that only advances i and keeps j the same
+        
+        As is, this works but is exponential, because at each given character (if they aren't the same),
+        there's two separate decision trees that occur, which could branch off into another set of decision trees,
+        2^n
+
+        You can memoize so at a given (i,j), we already know the max subsequence that can occur and return that
+
+        Memoized time complexity: O(M*N)
+        Space: O(M*N)
+        */
+        let i = 0
+        let j = 0
+        let res = 0
+        let memo = {}
+        const search = (i,j) => {
+            const key = `${i},${j}`
+            if (i === text1.length){
+                return 0
+            }
+            if (j === text2.length){
+                return 0
+            }
+            if (key in memo){
+                return memo[key]
+            }
+            if (text1[i] === text2[j]){
+                memo[key] = 1 + search(i+1,j+1)
+                return memo[key]
+            }
+            // return the max you can get by either 
+            // searching on the left or the right in the case
+            // that both strings are not equal
+            // try incrementing the index text1 only
+            const searchText1 = search(i+1, j)
+            // try incrementing the index for text2 only
+            const searchText2 = search(i, j+2)
+            memo[key] = Math.max(searchText1, searchText2)
+            return memo[key]
+        }
+
+        return search(0,0)
+    }
+
 /**
  * @param {string} text1
  * @param {string} text2
