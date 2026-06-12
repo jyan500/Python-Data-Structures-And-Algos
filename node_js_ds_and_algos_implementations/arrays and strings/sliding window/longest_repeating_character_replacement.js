d3da6aa115621d773b119dc6422a509a714588bc
jyan500 <jyan500@gmail.com>
2026-06-12 15:06:00 -0700

    longest repeating character replacement

--- node_js_ds_and_algos_implementations/arrays and strings/sliding window/longest_repeating_character_replacement.js
+++ node_js_ds_and_algos_implementations/arrays and strings/sliding window/longest_repeating_character_replacement.js
@@ -1,3 +1,66 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        /*
+        6/12/2026
+        sliding window
+        we keep a hashmap of the current counts of each character in the window
+        we check what the max count character is, meaning as long as we have enough k
+        replacements to equal the sum of all other characters (besides the max count character),
+        this is still a valid window
+
+
+        AABACCAAC k = 2
+        you can replace index 2 and index 4 with A since at index 4, max(character count) is 3 which
+        belongs to A
+
+        what happens when the amount of characters you want to replace exceeds k?
+        use a while loop that iterates the left pointer until the amount of character is less than k
+
+        note that if we know the max character count, all we need to do to find out all the other
+        characters we need to replace is to do our current window length - max character count,
+        since that would yield the total amount of replacements we need (rather than iterating
+        our hashmap and summing the other numbers together)
+
+        Time: O(N*26)
+        Space: O(26)
+
+        since there are only 26 possible uppercase letters, the input is bound by this number
+        */
+
+        let counter = new Map()
+        let l = 0
+        let res = 0
+        for (let r = 0; r < s.length; ++r){
+            counter[s[r]] = (s[r] in counter ? counter[s[r]]+1: 1)
+            // find the max count
+            let curMax = Math.max(...Object.values(counter))
+            // we need to replace the other characters that aren't the max count
+            let numReplacements = (r - l + 1) - curMax
+            if (numReplacements > k){
+                while (true){
+                    if (r-l+1 - curMax <= k){
+                        break
+                    }
+                    curMax = Math.max(...Object.values(counter))
+                    counter[s[l]]--
+                    if (counter[s[l]] === 0){
+                        delete counter[s[l]]
+                    }
+                    ++l
+                }
+            }
+            // after the replacements, save the window size
+            res = Math.max(res, (r-l+1))
+        }
+        return res
+    }
+}
+
 /**
  * @param {string} s
  * @param {number} k
