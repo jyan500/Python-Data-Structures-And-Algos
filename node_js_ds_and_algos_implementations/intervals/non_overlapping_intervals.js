b8a6b129d490a32270e76d0e34f292df40d0c75b
jyan500 <jyan500@gmail.com>
2026-06-30 12:33:44 -0700

    non overlapping intervals

--- node_js_ds_and_algos_implementations/intervals/non_overlapping_intervals.js
+++ node_js_ds_and_algos_implementations/intervals/non_overlapping_intervals.js
@@ -1,3 +1,61 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number}
+     */
+    eraseOverlapIntervals(intervals) {
+        /*
+        Alternate solution 6/30/2026
+        Greedy approach simplifies things here
+        Instead of sorting by start time (like in most interval problems),
+        sort by end time
+
+        The reasoning is because any interval that comes earlier in the list
+        is less likely to collide with future intervals, since it's an earlier
+        end time. So by default, we would be removing less intervals this way (resulting 
+        in the minimum)
+
+        The overlapping check would instead be if prev.end > cur.start since it's sorted
+        by end time
+
+        Loop through intervals starting from 1 ... n
+            If prevEnd > curStart
+                update remove count, but keep prev the same, since
+                it's a smaller end time, so we decrease the chances of conflicts
+            else
+                update prev to be the current interval
+
+        Time: O(NLogN)
+        Space: O(1)
+        */
+
+        // sort by end time
+        intervals.sort((a,b) => {
+            if (a[1] < b[1]){
+                return -1
+            }
+            else if (a[1] > b[1]){
+                return 1
+            }
+            return 0
+        })
+        let res = 0
+        let prev = intervals[0]
+        for (let i = 1; i < intervals.length; ++i){
+            let [prevStart, prevEnd] = prev
+            let [curStart, curEnd] = intervals[i]
+            // overlapping
+            if (prevEnd > curStart){
+                res++
+            }
+            else {
+                prev = intervals[i]
+            }
+        }
+        return res
+    }
+}
+
 /**
  * @param {number[][]} intervals
  * @return {number}
