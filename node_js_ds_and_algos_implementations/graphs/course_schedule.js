8997c29670024080306595fa00a5489e57451ae4
jyan500 <jyan500@gmail.com>
2026-07-03 22:06:27 -0700

    course schedule

--- node_js_ds_and_algos_implementations/graphs/course_schedule.js
+++ node_js_ds_and_algos_implementations/graphs/course_schedule.js
@@ -1,3 +1,62 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @return {boolean}
+     */
+    canFinish(numCourses, prerequisites) {
+        /* 
+        slightly different version that uses two sets, one a global set
+        which tracks whether we've seen a node already, and a per-path cycle set which
+        adds and then removes it so other paths can potentially visit this node, 
+        but if the same course is visited within the path, it gets marked as a cycle.
+        The global visited set avoids repeated work, because if a node has already been seen,
+        we already know it's not a cycle
+        */
+        let adjacency = {}
+        for (let i = 0; i < numCourses; ++i){
+            adjacency[i] = new Set()
+        }
+        for (let [course, prereq] of prerequisites){
+            adjacency[course].add(prereq)
+        }
+
+        let cycles = new Set()
+        let visited = new Set()
+        const dfs = (node) => {
+            // if the current path re-visits the same node, this is a cycle
+            if (cycles.has(node)){
+                return false
+            }
+            // keep track of a global visited set that confirms that we've 
+            // already seen this node in a separate path, and if we haven't
+            // returned false above, that means it's not a cycle so we don't need to redo
+            if (visited.has(node)){
+                return true
+            }
+            visited.add(node)
+            cycles.add(node)
+            for (let neighbor of adjacency[node]){
+                if (!dfs(neighbor)){
+                    return false
+                }
+            }
+            // since this is a per-path visited set, we have to pop out
+            // so that other courses that may have this current course as a 
+            // prereq don't get excluded
+            cycles.delete(node)
+            return true
+        }
+        // need to iterate through all courses to account for disconnected components
+        for (let i = 0; i < numCourses; ++i){
+            if (!visited.has(i) && !dfs(i)){
+                return false
+            }
+        }
+        return true
+    }
+}
+
 /**
  * @param {number} numCourses
  * @param {number[][]} prerequisites
