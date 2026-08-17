74d8f45e90f912904ebb35b4dcd2021fb4ee440c
jyan500 <jyan500@gmail.com>
2026-08-17 11:27:14 -0700

    subtree of another tree

--- node_js_ds_and_algos_implementations/trees/subtree_of_another_tree.js
+++ node_js_ds_and_algos_implementations/trees/subtree_of_another_tree.js
@@ -1,3 +1,36 @@
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {TreeNode} subRoot
+     * @return {boolean}
+     */
+    isSubtree(root, subRoot) {
+        /* 
+            Revisited 8/17/2026
+            applying isSameTree on each node?
+            time: O(M*N)
+            space: O(M*N)
+        */
+        const isSameTree = (p, q) => {
+            if (!p && !q){
+                return true
+            }
+            if (p && !q || !p && q){
+                return false
+            }
+            else {
+                return p.val === q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+            }
+        }
+        if (root){
+            if (isSameTree(root,subRoot)){
+                return true
+            }
+            return this.isSubtree(root.left, subRoot) || this.isSubtree(root.right, subRoot)
+        }
+        return false
+    }
+
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
